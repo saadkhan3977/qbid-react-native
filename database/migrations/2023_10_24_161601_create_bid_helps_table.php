1f44decd3a3fae286d9a49dcab51ddc28d95d17f
Saad Awan <saadkhan3977@gmail.com>
2023-11-23 23:57:09 +0500

    backup

---
+++ database/migrations/2023_10_24_161601_create_bid_helps_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bid_helps', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->nullable();
+            $table->string('bid_name')->nullable();
+            $table->string('service_type')->nullable();
+            $table->text('description')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bid_helps');
+    }
+};
